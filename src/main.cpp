a326da259aff43e23a358db870e1d7214b109d02
Sasha <alexburba53@gmail.com>
2025-05-26 22:33:40 +0300

    Task 15

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void showMenu() {
     while (true) {
         cout << "\n=== TIC-TAC-TOE GAME ===\n";
+        cout << "\n=== TIC-TAC-TOE GAME ===\n";
         cout << "1. Start Game\n";
         cout << "1. Start Game\n";
         cout << "2. Settings\n";
